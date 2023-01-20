aadaf5c4dc5890956b7c0ec31570a121db22d336
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-01-20 13:53:48 +0530

    chnaged hit per page to 500

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends React.Component {
     })
 
     return fetch(
-      `https://hn.algolia.com/api/v1/${this.state.searchBy}?query=${this.state.query}&hitsPerPage=1000&page=${this.state.page}&tags=${this.state.searchType}`
+      `https://hn.algolia.com/api/v1/${this.state.searchBy}?query=${this.state.query}&hitsPerPage=500&page=${this.state.page}&tags=${this.state.searchType}`
     )
       .then((response) => {
         if (response.ok) {
