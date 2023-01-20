4e16cf23487e5fc7d3a25dfcefe5264946e01201
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-01-20 16:19:07 +0530

    added line spaces in between curly braces

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,34 +1,60 @@
 function Comments(props) {
   return (
-    <div className="news" key={props.objectID}>
+    <div className="news" key={
+      props.objectID
+    }>
       <div className="news-details">
         <span>
-          <a href={"https://news.ycombinator.com/item?id=" + props.objectID}>
-            {props.points || 0} points
+
+          <a href={
+            "https://news.ycombinator.com/item?id=" + props.objectID
+          }>
+
+            {
+              props.points || 0
+            } points
+
+          </a>
+        </span>
+
+        <span> | </span>
+
+        <span>
+          <a href={
+            "https://news.ycombinator.com/user?id=" + props.author
+          }>
+            {
+              props.author
+            }
           </a>
         </span>
         <span> | </span>
         <span>
-          <a href={"https://news.ycombinator.com/user?id=" + props.author}>
-            {props.author}
-          </a>
-        </span>
-        <span> | </span>
-        <span>
-          <a href={"https://news.ycombinator.com/item?id=" + props.parent_id}>
+          <a href={
+            "https://news.ycombinator.com/item?id=" + props.parent_id
+          }>
             parent
           </a>
         </span>
         <span> | </span>
         <span>
           <span>on: </span>
-          <a href={"https://news.ycombinator.com/item?id=" + props.story_id}>
-            {props.story_title}
+          <a href={
+            "https://news.ycombinator.com/item?id=" + props.story_id
+          }>
+            {
+              props.story_title
+            }
           </a>
         </span>
       </div>
+
       <div
-        dangerouslySetInnerHTML={{ __html: props.comment_text }}
+        dangerouslySetInnerHTML={
+          {
+            __html: props.comment_text
+          }
+        }
       />
     </div>
   );
