c97ba036acfcc1aba28867ad5be0d97f896eef99
vamsi krishna <vamsi.bandaru.22.1@mountblue.tech>
2023-01-20 11:13:03 +0530

    modified: header js file to add title

--- src/jsFiles/Header.js
+++ src/jsFiles/Header.js
@@ -1,28 +1,25 @@
 import React from "react";
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <header>
-        <img src="/images/logo.webp"></img>
-        <p className="logo-name">
-          Search <br /> Hacker News
-        </p>
-        <div className="search-container">
-          <div className="image-container">
-            <div className="search-icon"></div>
-          </div>
-          <input
-            placeholder="Search stories by title, url or author"
-            onKeyDown={this.props.handleKeyPress}
-          ></input>
-        </div>
-      </header>
-    );
-  }
+   constructor(props) {
+      super(props);
+   }
+   render() {
+      return (
+         <header>
+            <img src="/images/logo.webp"></img>
+            <p className="title"> Hacker News
+            </p>
+            <div className="search-box">
+               <div className="search-icon"></div>
+               <input
+                  placeholder="Search stories by title, url or author"
+                  onKeyDown={this.props.handleKeyPress}
+               ></input>
+            </div>
+         </header>
+      );
+   }
 }
 
 export default Header;
